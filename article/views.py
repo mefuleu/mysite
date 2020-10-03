0e6137c62d269a1cf4c5ccbd5b3dab19b1abff50
jackeyhate <1217654940@qq.com>
2020-10-03 19:22:14 +0800

    first

--- article/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def showArticle(request):
-    return render(request,'article.html')
--- music/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def showMusic(request):
-    return render(request,'music.html')
