a4c00bf70d3ee891e3723b2ed3d499548f5cd7eb
jackeyhate <1217654940@qq.com>
2020-12-12 14:15:12 +0800

    http协议下的chat

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.chat_index, name='chat_index'),
-path('<str:room_name>/', views.chat_room, name='room'),
+    path('<str:room_name>/', views.chat_room, name='room'),
 ]
--- mysite/asgi.py
+++ mysite/asgi.py
@@ -7,14 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-import os
-
+import os,django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.development")
+django.setup()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.production")
+
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
